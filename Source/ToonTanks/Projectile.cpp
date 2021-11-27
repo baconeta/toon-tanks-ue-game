f76f42fbd7f8f17f089b6b0c75164955c86dac44
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2021-11-27 19:57:01 +0200

    add particle effects

--- Source/ToonTanks/Projectile.cpp
+++ Source/ToonTanks/Projectile.cpp
@@ -45,6 +45,7 @@ void AProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimi
 	if (OtherActor && OtherActor != this && OtherActor != MyOwner)
 	{
 		UGameplayStatics::ApplyDamage(OtherActor, Damage, MyOwnerInstigator, this, DamageTypeClass);
+		UGameplayStatics::SpawnEmitterAtLocation(this, HitParticles, GetActorLocation(), GetActorRotation());
 		Destroy();
 	}
 }
